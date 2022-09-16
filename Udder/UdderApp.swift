3860745c128ffbf08e9abad5a58cabe20e159280
Mykhaylo Kotyk <mikl.kotyk@gmail.com>
2022-09-16 12:49:39 +0300

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by admin on 16.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
